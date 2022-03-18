09b0f95fba01e4a961a1258ea5985d0243dacd2c
Eduardo <lalolai19@gmail.com>
2022-03-17 18:33:31 -0600

    getGift.js mod

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -2,18 +2,18 @@
 
 export const getGifs = async (categoria) => {
 
-    const url = `http://api.giphy.com/v1/gifs/search?api_key=WPreGqhzSLv8xxJgkRwIx9TiLw0XVm2g&q=${encodeURI(categoria)}&limit=10`
-    const resp = await fetch(url);
-    const { data } = await resp.json();
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=WPreGqhzSLv8xxJgkRwIx9TiLw0XVm2g&q=${encodeURI(categoria)}&limit=10`
+  const resp = await fetch(url);
+  const { data } = await resp.json();
 
-    const gifs = data.map(img => {
-      return {
-        id: img.id,
-        title: img.title,
-        url: img.images?.downsized_medium.url
-      }
-    })
+  const gifs = data.map(img => {
+    return {
+      id: img.id,
+      title: img.title,
+      url: img.images?.downsized_medium.url
+    }
+  })
 
-    return gifs;
+  return gifs;
 
-  }
+}
